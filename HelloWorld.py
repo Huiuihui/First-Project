bc0055471ab720f54cb1317b93a0aeca3831799f
Torsten Igel <toddy0815@gmx.de>
2014-06-04 22:35:48 +0200

    First commit via terminal, minor changes

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,8 +6,14 @@ file = open('HelloWorld.txt', "w")
 file.write("Hello world in the new file\n")
 file.write("and another line\n")
 file.write("like in the new world\n")
+file.write("I love Andrea")
 file.close()
 
 f = open('HelloWorld.txt', 'r+')
+
+print f.readline()
 print f.read()
+
+
+
 	
