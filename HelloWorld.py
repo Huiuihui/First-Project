df4fd18f15b0e5a88263890111420187c87df4a4
Torsten Igel <toddy0815@gmx.de>
2014-06-04 21:24:27 +0200

    First Project
    Added the description for github

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+
+file = open('HelloWorld.txt', "w")
+file.write("Hello world in the new file\n")
+file.write("and another line\n")
+file.write("like in the new world\n")
+file.close()
+
+f = open('HelloWorld.txt', 'r+')
+print f.read()
+	
